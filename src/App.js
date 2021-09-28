7a0263f195d4b622bdeb8fd585946c471455d978
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-09-28 03:17:03 -0400

    1:30

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import WebcamCapture from "./WebcamCapture";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Preview from "./Preview";
 
 function App() {
   return (
@@ -9,6 +10,11 @@ function App() {
       <Router>
         <div className="app__body">
           <Switch>
+
+            <Route  path="/preview">
+              <Preview />
+            </Route>
+
             <Route exact path="/">
               <WebcamCapture />
             </Route>
